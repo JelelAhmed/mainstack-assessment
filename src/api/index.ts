fe8135872f0f634cec3af21991785878e19bb78f
jelelahmed <abduljelelahmed@gmail.com>
2025-10-20 00:36:25 +0100

    feat: create api layer for data-fetching

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+// src/api/index.ts
+import { User } from "../types";
+import { Wallet } from "../types";
+import { Transaction } from "../types/transactions";
+
+const BASE_URL = "https://fe-task-api.mainstack.io";
+
+export const fetchUser = async (): Promise<User> => {
+  const res = await fetch(`${BASE_URL}/user`);
+  if (!res.ok) throw new Error("Failed to fetch user");
+  return res.json();
+};
+
+export const fetchWallet = async (): Promise<Wallet> => {
+  const res = await fetch(`${BASE_URL}/wallet`);
+  if (!res.ok) throw new Error("Failed to fetch wallet");
+  return res.json();
+};
+
+export const fetchTransactions = async (): Promise<Transaction[]> => {
+  const res = await fetch(`${BASE_URL}/transactions`);
+  if (!res.ok) throw new Error("Failed to fetch transactions");
+  return res.json();
+};
